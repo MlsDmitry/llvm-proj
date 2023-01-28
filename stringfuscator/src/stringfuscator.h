7d43f573f0ee798bf03e69009a81bc79f5f6e071
mlsdmitry <43723140+MlsDmitry@users.noreply.github.com>
2023-01-29 02:31:21 +0300

    Unused stuff

--- stringfuscator/src/stringfuscator.h
+++ stringfuscator/src/stringfuscator.h
@@ -1,7 +1,7 @@
-//__FILENAME__ \n Written by programmer
+// //__FILENAME__ \n Written by programmer
 
-#ifndef stringfuscator_h
-#define stringfuscator_h
+// #ifndef stringfuscator_h
+// #define stringfuscator_h
 
 
-#endif /* stringfuscator_h */
+// #endif /* stringfuscator_h */
